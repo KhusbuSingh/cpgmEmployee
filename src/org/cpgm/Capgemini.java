be3ba539961888523b737b54fdf791fe2305dbf0
KhusbuSingh <skhusbu050@gmail.com>
2023-10-25 17:14:58 +0530

    code

---
+++ src/org/cpgm/Capgemini.java
@@ -0,0 +1,39 @@
+package org.cpgm;
+
+
+
+public class Capgemini {
+	private void empId() {
+		System.out.println("emp id is 1001");
+	 System.out.println("subemployee id is 3377");
+	}
+	private void empNAME() {
+		System.out.println("emp name is saroj");
+    System.out.println("subemployee name is khushi");
+	}
+	private void empDob() {
+		System.out.println("emp dob is 02-02-1979");
+  System.out.println("subemp dob id 03-07-97");
+	}
+	private void empPhone() {
+		System.out.println("emp phone is 123456");
+		
+
+	}
+	private void empEmail() {
+		System.out.println("Employee1");
+		
+		
+
+	}
+	public static void main(String[] args) {
+		Capgemini e= new Capgemini();
+		e.empId();
+		e.empNAME();
+		e.empDob();
+		e.empPhone();
+		e.empEmail();
+		
+	}
+
+}
